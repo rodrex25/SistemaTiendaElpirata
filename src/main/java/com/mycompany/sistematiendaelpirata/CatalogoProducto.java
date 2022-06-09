7d0d2d7d5d20a6ad0564c91edaeace2fe812079a
rodrex25 <gabresh25@gmail.com>
2022-06-08 22:40:16 -0500

    feat: se agregaronlos txt y la clase 'Catalogo producto'

---
+++ src/main/java/com/mycompany/sistematiendaelpirata/CatalogoProducto.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistematiendaelpirata;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author JuanA
+ */
+public class CatalogoProducto implements Iterable<Producto> {
+
+    //Atributos
+    
+    private ArrayList<Producto> productos;
+    
+    //Metodos
+
+    public CatalogoProducto() {
+        productos = new ArrayList<Producto>();
+    }
+
+    public void addProducto(Producto product) {
+        productos.add(product);
+    }
+
+    public Producto getProducto(String idProducto) {
+        for (Iterator<Producto> listProduct = productos.iterator(); listProduct.hasNext();){
+            Producto producto = listProduct.next();
+            if (producto.getIdProducto().equals(idProducto)) {
+                return producto;
+            }
+        }
+        return null;
+    }
+
+    //iterador
+
+    @Override
+    public Iterator<Producto> iterator() {
+        return productos.iterator();
+    }
+}
